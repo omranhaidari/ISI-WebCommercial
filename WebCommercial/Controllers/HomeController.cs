c07799e0fde545a9f3c68ec1a28561a4a1c4ae3c
Unknown <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2018-01-24 16:47:01 +0100

    Code de l'appli lancé
    Bon, alors y a vraiment tout dans ce commit (comme ça vous n'avez pas besoin de télécharger le code du prof) mais je pense que certains dossiers doivent passer dans le gitignore (sauf que je ne sais pas lesquels).
    J'ai fait 2-3 modifs (mineures) pour tester un peu le fonctionnement et ça a l'air plutôt tranquille (pour les vues en tout cas).

---
+++ WebCommercial/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCommercial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Accueil";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            ViewBag.Message = "Page en construction";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            ViewBag.Message = "Page de contact";
+
+            return View();
+        }
+
+        public ActionResult Accueil(string id = "Christian")
+        {
+            ViewBag.Title = "Accueil";
+            if (string.IsNullOrWhiteSpace(id))
+                return View("Error");
+            else
+            {
+                ViewBag.Nom = id;
+                return View();
+            }
+        }
+    }
+}
